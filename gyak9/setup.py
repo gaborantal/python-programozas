73d3e6c9318c4c0e3406282b937a60e948966082
Dépanneur <antal@inf.u-szeged.hu>
2022-04-26 12:59:41 +0200

    Add gyak9

---
+++ gyak9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-programozas-gyak9',
+    version='',
+    packages=find_packages(exclude=("tests",)),
+    url='',
+    license='',
+    author='antalg',
+    author_email='',
+    description=''
+)
